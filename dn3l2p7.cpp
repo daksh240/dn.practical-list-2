e24816e51ca058ec76f3cc328755d7e3d1853146
daksh240 <141892611+daksh240@users.noreply.github.com>
2023-08-24 09:08:10 +0530

    Create dn3l2p7.cpp

---
+++ dn3l2p7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  
+using namespace std;  
+enum week { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };  
+int main()  
+{  
+    week day;  
+    day = Sunday;  
+    cout << "Day:- " << day+1<<endl; 
+	
+  	cout<<"daksh nanera 08"; 
+    return 0;  
+}     
