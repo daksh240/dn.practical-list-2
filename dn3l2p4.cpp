b55b7866ad0b84ffd65f79f497d3342cd330bada
daksh240 <141892611+daksh240@users.noreply.github.com>
2023-08-24 08:21:30 +0530

    Create dn3l2p4.cpp

---
+++ dn3l2p4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>       
+#include <iomanip>      
+using namespace std;  
+  
+int main () 
+{  
+  cout << setw(10);  
+  cout << 24 << endl; 
+
+  cout<<"daksh nanera 08";
+  return 0;  
+}   
