ab457d2fa5237ac42ee7b033ee62e2f6b41f8a80
daksh240 <141892611+daksh240@users.noreply.github.com>
2023-08-24 09:07:38 +0530

    Create dn3l2p6.cpp

---
+++ dn3l2p6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>      
+#include <iomanip>       
+using namespace std;  
+  
+int main () 
+{  
+  cout<< setfill ('x') << setw (10);  
+  cout<< 100<< endl;  
+
+  cout<<"daksh nanera 08";
+  return 0;  
+}  
