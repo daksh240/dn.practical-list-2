ca11327f2cf06c78857c0bb09346f1ccde00664d
daksh240 <141892611+daksh240@users.noreply.github.com>
2023-08-24 09:06:29 +0530

    Create dn3l2p5.cpp

---
+++ dn3l2p5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>     
+#include <iomanip>       
+using namespace std;  
+  
+int main () 
+{  
+  double f =3.14159;  
+  cout << setprecision(5) << f << '\n';  
+  cout << setprecision(9) << f << '\n';  
+  cout << fixed;  
+  cout << setprecision(5) << f << '\n';  
+  cout << setprecision(9) << f << '\n';  
+
+  cout<<"daksh nanera 08";
+  return 0;  
+}  
