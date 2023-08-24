bc7bb13d6fd302159fcc79ddedb9e2bf27b1fcb2
daksh240 <141892611+daksh240@users.noreply.github.com>
2023-08-24 08:18:11 +0530

    Create dn3l2p1.cpp

---
+++ dn3l2p1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  
+using namespace std;  
+int num = 50;  
+int main ()  
+{  
+  
+	int num = 100;  
+  
+
+	cout<<" The value of the local variable num: "<<num;  
+  
+ 
+	cout<<"\n The value of the global variable num: "<<::num;   
+
+	return 0;  
+}  
