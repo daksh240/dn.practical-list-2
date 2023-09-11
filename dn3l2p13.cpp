7211bdef367363b14084f3f78ec17672fbd1d692
daksh240 <141892611+daksh240@users.noreply.github.com>
2023-09-11 11:15:50 +0530

    Add files via upload

---
+++ dn3l2p13.cpp
@@ -0,0 +1,14 @@
+//write a c++ program to demonstrate use of constant argument.
+#include<iostream>
+using namespace std;
+int main()
+{
+	const int a=20;
+	const int b=25;
+	int c;
+	
+	c=a+b;
+	cout<<"the sum of a and b is::"<<c<<endl;
+	
+	return 0;
+}
---
+++ dn3l2p9.cpp
@@ -0,0 +1,15 @@
+//write a c++ program to add two number usong scope resolution operator.
+#include<iostream>
+using namespace std;
+int num1,num2;
+int main()
+{
+	cout<<"enter the num1::";
+	cin>>num1;
+	cout<<"enter the num2::";
+	cin>>num2;
+	
+	cout<<"The Sum is ::"<<num1+::num2<<endl<<endl;
+	
+	cout<<"daksh nanera 08";
+}
