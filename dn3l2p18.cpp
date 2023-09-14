5b5a2e1689e81af09c1bf3c979d4ee07769626eb
daksh240 <141892611+daksh240@users.noreply.github.com>
2023-09-14 08:50:03 +0530

    Create dn3l2p18.cpp

---
+++ dn3l2p18.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+float areaOfCircle(float radius_circle);
+int main()
+{
+   	float radius;
+   	cout<<"Enter the radius of circle :: ";
+   	cin>>radius;
+   	cout<<"Area of circle :: "<<areaOfCircle(radius)<<endl<<endl;
+   	
+   	cout<<"daksh nanera 08"<<endl;
+   	return 0;
+}
+float areaOfCircle(float radius_circle)
+{
+   	float area_circle;
+   	area_circle=3.14*radius_circle*radius_circle;
+	return area_circle;
+   
+
+}
